c0f80cd4000b07ccb23d95dc99fb1d212a445426
kuujinbo <kuujinbo@gmail.com>
2016-03-26 15:04:02 -0700

    added CSS support to HTML => PDF conversion with vertical text example

--- src/ProgramCode/XmlWorkers/CustomTdProcessor.cs
+++ src/ProgramCode/XmlWorkers/CustomTdProcessor.cs
@@ -49,17 +49,38 @@ namespace kuujinbo.StackOverflow.iTextSharp.ProgramCode.XmlWorkers
 
     public class CustomTdProcessor
     {
+        string OUTPUT_FILE;
+        string XHTML = @"
+        <h1>Table with Vertical Text</h1>
+        <table><tr>
+        <td style='writing-mode:sideways-lr;text-align:center;width:40px;'>First</td>
+        <td style='writing-mode:sideways-lr;text-align:center;width:40px;'>Second</td></tr>
+        <tr><td style='text-align:center'>1</td>
+        <td style='text-align:center'>2</td></tr></table>
+
+        <h1>Table <u>without</u> Vertical Text</h1>
+        <table width='50%'>
+        <tr><td class='light-yellow'>0</td></tr>
+        <tr><td>1</td></tr>
+        <tr><td class='light-yellow'>2</td></tr>
+        <tr><td>3</td></tr>
+        </table>";
+
+        string CSS = @"
+            body {font-size: 12px;}
+            table {border-collapse:collapse; margin:8px;}
+            .light-yellow {background-color:#ffff99;}
+            td {border:1px solid #ccc;padding:4px;}
+        ";
+
         public void Go()
         {
-            var OUTPUT_FILE = Helpers.IO.GetClassOutputPath(this);
+            OUTPUT_FILE = Helpers.IO.GetClassOutputPath(this);
+            ConvertHtmlToPdf(XHTML, CSS);
+        }
 
-            string XHTML = @"
-            <table border='1'><tr>
-            <td style='writing-mode:sideways-lr;text-align:center;width:40px;'>First</td>
-            <td style='writing-mode:sideways-lr;text-align:center;width:40px;'>Second</td></tr>
-            <tr><td style='text-align:center'>1</td>
-            <td style='text-align:center'>2</td></tr></table>";
-
+        public void ConvertHtmlToPdf(string xHtml, string css)
+        {
             using (var stream = new FileStream(OUTPUT_FILE, FileMode.Create))
             {
                 using (var document = new Document())
@@ -67,24 +88,28 @@ namespace kuujinbo.StackOverflow.iTextSharp.ProgramCode.XmlWorkers
                     var writer = PdfWriter.GetInstance(document, stream);
                     document.Open();
 
+                    // instantiate custom tag processor and add to `HtmlPipelineContext`.
                     var tagProcessorFactory = Tags.GetHtmlTagProcessorFactory();
                     tagProcessorFactory.AddProcessor(
                         new TableDataProcessor(), 
                         new string[] { HTML.Tag.TD }
                     );
-
                     var htmlPipelineContext = new HtmlPipelineContext(null);
                     htmlPipelineContext.SetTagFactory(tagProcessorFactory);
 
                     var pdfWriterPipeline = new PdfWriterPipeline(document, writer);
                     var htmlPipeline = new HtmlPipeline(htmlPipelineContext, pdfWriterPipeline);
+
+                    // get an ICssResolver and add the custom CSS
+                    var cssResolver = XMLWorkerHelper.GetInstance().GetDefaultCssResolver(true);
+                    cssResolver.AddCss(css, "utf-8", true);
                     var cssResolverPipeline = new CssResolverPipeline(
-                        XMLWorkerHelper.GetInstance().GetDefaultCssResolver(true), 
-                        htmlPipeline
+                        cssResolver, htmlPipeline
                     );
+
                     var worker = new XMLWorker(cssResolverPipeline, true);
                     var parser = new XMLParser(worker);
-                    using (var stringReader = new StringReader(XHTML))
+                    using (var stringReader = new StringReader(xHtml))
                     {
                         parser.Parse(stringReader);
                     }
